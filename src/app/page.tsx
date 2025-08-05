c3c2315f4fa5a1fa5c0e22c6a9f271f65cc83b28
Noah Milberger <n.milberger@gmail.com>
2025-08-05 15:01:07 -0400

    Small copy update.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { H1, P } from "@/components/ui/typography";
 export default function Home() {
   return (
     <div className="flex flex-col items-center gap-6">
-      <H1>Morgan + Noah</H1>
+      <H1>Morgan & Noah</H1>
       <P className="text-2xl font-medium">Summer 2026</P>
     </div>
   );
