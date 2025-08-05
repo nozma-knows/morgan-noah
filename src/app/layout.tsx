e7f3a5b5a1ffdefe69cee8fc568b2bdca6cd0822
Noah Milberger <n.milberger@gmail.com>
2025-08-05 14:37:25 -0400

    Added shadcn and removed default page.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="h-screen w-screen flex justify-center items-center">
+          {children}
+        </div>
       </body>
     </html>
   );
